886c6bd387ae6f4f6744b5df848f345830e95a74
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-03-02 13:58:16 +0100

    fix vercel v3

--- src/database/schemas/cat.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const CatSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    age: Number,
-    breed: {type: String, required: true},
-});
