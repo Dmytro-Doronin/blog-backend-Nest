87053a760883ab7288471c4733e05461b71a1114
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-09-24 13:45:32 +0200

    fix options request

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,35 @@ import {mainAppSettings} from "./settings/main-app-settings";
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);  // Отвечаем на предзапрос
+    } else {
+      next();
+    }
+  });
 
   mainAppSettings(app)
-
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:4200',
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    preflightContinue: false,  // Останавливает предзапросы на уровне CORS
+    optionsSuccessStatus: 204  // Устанавливает успешный код ответа для OPTIONS
   });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
+
+
 
   await app.listen(3000);
 }
