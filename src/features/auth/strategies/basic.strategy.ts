091dabe32f0258293a53c6f270f47738b3386b35
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-10-13 12:05:36 +0200

    BasicAuthStrategy check users credantials

--- src/features/auth/strategies/basic.strategy.ts
+++ src/features/auth/strategies/basic.strategy.ts
@@ -1,17 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { BasicStrategy } from 'passport-http';
+import {AuthService} from "../service/auth.service";
 
 @Injectable()
 export class BasicAuthStrategy extends PassportStrategy(BasicStrategy) {
-    constructor() {
+    constructor(private readonly authService: AuthService) {
         super();
     }
 
-    validate(username: string, password: string): any {
-        const isValid = (username === 'admin' && password === 'qwerty');
+    async validate(username: string, password: string): Promise<any> {
+        // const isValid = (username === 'admin' && password === 'qwerty');
+        const user = await this.authService.validateUser(username, password);
 
-        if (!isValid) {
+        if (!user) {
             throw new UnauthorizedException('Invalid credentials');
         }
         return { username };
