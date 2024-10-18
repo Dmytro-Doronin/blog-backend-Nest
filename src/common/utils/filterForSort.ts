9c13cb5d212f5513b314643c6b6b0e13e981ada0
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-10-18 19:14:04 +0200

    change sort function

--- src/common/utils/filterForSort.ts
+++ src/common/utils/filterForSort.ts
@@ -1,8 +1,16 @@
 
-export const filterForSort = (sortBy: string, sortDirection: string ): {[key: string]: 1 | -1} => {
+// export const filterForSort = (sortBy: string, sortDirection: string ): {[key: string]: 1 | -1} => {
+//     if (sortDirection === 'asc') {
+//         return {[`accountData.${sortBy}`]: 1}
+//     } else {
+//         return {[`accountData.${sortBy}`]: -1 }
+//     }
+// }
+
+export const filterForSort = (sortBy: string, sortDirection: string): { [key: string]: 1 | -1 } => {
     if (sortDirection === 'asc') {
-        return {[`accountData.${sortBy}`]: 1}
+        return { [sortBy]: 1 }  // Используем само поле без accountData
     } else {
-        return {[`accountData.${sortBy}`]: -1 }
+        return { [sortBy]: -1 } // Используем само поле без accountData
     }
 }
