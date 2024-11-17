a21bdbf0660352da2eb641ee9324779d2cb43ee4
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-11-17 15:18:27 +0100

    new url for cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
 
     if (req.method === 'OPTIONS') {
-      res.sendStatus(204);  
+      res.sendStatus(204);
     } else {
       next();
     }
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.use(cookieParser());
   mainAppSettings(app)
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://blog-frontend-angular-eight.vercel.app',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
