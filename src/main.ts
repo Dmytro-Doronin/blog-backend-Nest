9c299bc945c1196d5053d7043db663be2d2211fb
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-03-18 21:36:12 +0100

    vercel url

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   // });
     app.use((req, res, next) => {
         if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.header('Access-Control-Allow-Origin', 'https://blog-frontend-angular-eight.vercel.app');
             res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
             res.header('Access-Control-Allow-Credentials', 'true');
