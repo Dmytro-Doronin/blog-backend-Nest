a9ef0fed12df47f01134d5880953b0d4f1097eca
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-04-20 09:25:29 +0200

    delete app use

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,16 @@ import cookieParser from "cookie-parser";
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-    app.use((req, res, next) => {
-        if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Origin', 'https://blog-frontend-angular-eight.vercel.app');
-            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-            res.header('Access-Control-Allow-Credentials', 'true');
-            return res.status(204).send();
-        }
-        next();
-    });
+    // app.use((req, res, next) => {
+    //     if (req.method === 'OPTIONS') {
+    //         res.header('Access-Control-Allow-Origin', 'https://blog-frontend-angular-eight.vercel.app');
+    //         res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    //         res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    //         res.header('Access-Control-Allow-Credentials', 'true');
+    //         return res.status(204).send();
+    //     }
+    //     next();
+    // });
   app.use(cookieParser());
   mainAppSettings(app)
   app.enableCors({
